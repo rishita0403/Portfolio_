65f3176aeae05084e3a2488aff50f7cb72a41dd6
rishita0403 <108778828+rishita0403@users.noreply.github.com>
2024-02-08 15:23:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const hamburgerButton=document.getElementById('hamburger')
+const navList=document.getElementById('nav-List')
+
+function toggleButton(){
+    navList.classList.toggle('show')
+}
+
+hamburgerButton.addEventListener('click',toggleButton)
+
+
+
